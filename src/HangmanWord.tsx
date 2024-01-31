a03355db8cc8cfe7d210034f30801ce38c52059e
William Chan <chan.william.ts@gmail.com>
2024-01-31 16:27:14 -0500

    Display word in HangmanWord component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,1 +1,20 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const word = "lohpoh";
+  const guessedLetters = ["l", "h"];
+
+  return (
+    <div className="flex gap-8 font-mono text-8xl font-bold uppercase">
+      {word.split("").map((letter, index) => (
+        <span className="border-b-8 border-black" key={index}>
+          <span
+            className={
+              guessedLetters.includes(letter) ? "visible" : "invisible"
+            }
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
