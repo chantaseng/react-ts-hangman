6c546040852d9421180afa73fc64f29678e88985
William Chan <chan.william.ts@gmail.com>
2024-01-31 14:11:07 -0500

    HangmanDrawing component

---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,10 @@
+export function HangmanDrawing() {
+  return (
+    <div className="relative">
+      <div className="absolute right-0 top-0 h-[50px] w-2.5 bg-black" />
+      <div className="ml-[120px] h-2.5 w-[200px] bg-black" />
+      <div className="ml-[120px] h-[400px] w-2.5 bg-black" />
+      <div className="h-2.5 w-[250px] bg-black" />
+    </div>
+  );
+}
